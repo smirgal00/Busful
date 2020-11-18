92fb1e424fea01780c13cc8ccdc4ce6aec6ad825
smirgal00 <georgeadam298@gmail.com>
2020-11-18 12:32:15 +0200

    small fix

--- app/src/main/java/com/example/busful/Routes.java
+++ app/src/main/java/com/example/busful/Routes.java
@@ -1,7 +1,5 @@
 package com.example.busful;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -11,5 +9,7 @@ public class Routes extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+
+
     }
 }
