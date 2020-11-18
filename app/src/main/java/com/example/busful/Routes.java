3d6d8d4293eda55cd2b0679f67e51c28a160e433
smirgal00 <georgeadam298@gmail.com>
2020-11-18 13:51:08 +0200

    maps

--- app/src/main/java/com/example/busful/Routes.java
+++ app/src/main/java/com/example/busful/Routes.java
@@ -1,15 +1,41 @@
 package com.example.busful;
 
-import android.app.Activity;
+import android.graphics.Camera;
 import android.os.Bundle;
 
-public class Routes extends Activity {
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Routes extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
 
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng belly = new LatLng(46.761107, 23.565309);
+        map.addMarker(new MarkerOptions().position(belly).title("Big Belly!"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(belly, 15));
+        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
     }
 }
