39238c9f47941f8309c0078479eca14ed4e63d13
smirgal00 <georgeadam298@gmail.com>
2020-12-12 23:14:46 +0200

    routes 99%

--- app/src/main/java/com/example/busful/Maps.java
+++ app/src/main/java/com/example/busful/Maps.java
@@ -1,15 +1,59 @@
 package com.example.busful;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class Maps extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Maps extends AppCompatActivity implements OnMapReadyCallback {
+
+    private List<LatLng> latLngList;
+    private String coords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        latLngList = new ArrayList<>();
+
+        coords = getIntent().getExtras().getString("routes");
+        parseString();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.addAll(latLngList);
+        polylineOptions.width(5).color(Color.RED);
+        googleMap.addPolyline(polylineOptions);
+    }
+
+    private void parseString() {
+        coords = coords.replace(" ", "");
+        String[] p1 = this.coords.split("\\+");
+        for (String s : p1) {
+            String[] aux = s.split(",");
+            latLngList.add(new LatLng(Double.parseDouble(aux[0]), Double.parseDouble(aux[1])));
+        }
+
     }
 }
--- app/src/main/java/com/example/busful/Routes.java
+++ app/src/main/java/com/example/busful/Routes.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Camera;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.AdapterView;
@@ -32,43 +33,44 @@ import com.google.firebase.database.ValueEventListener;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Routes extends Activity {
-
-    private GoogleMap map;
-    private DatabaseReference databaseReference;
-    private List<String> routeList;
-    private ListView listView;
     private Intent intent;
+    private String name;
+    private Map<String, String> value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
-        listView = (ListView) findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(ArrayAdapter.createFromResource(this, R.array.busSpinner,
                 android.R.layout.simple_list_item_1));
+        value = new HashMap<>();
 
         intent = new Intent(this, Maps.class);
 
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            String name = parent.getItemAtPosition(position).toString();
-            readData(name);
-            startActivity(intent);
-        });
-    }
-
-
-    public void readData(String nr) {
-        databaseReference = FirebaseDatabase.getInstance()
-                .getReference("bus_info")
-                .child(nr);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance()
+                .getReference("bus_info");
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                intent.putExtra("route", snapshot.getChildren().toString());
+                for (DataSnapshot s : snapshot.getChildren()) {
+                    for (DataSnapshot s1 : s.getChildren()) {
+                        value.put(s.getKey(), s1.getValue().toString());
+                    }
+                }
+
+                listView.setOnItemClickListener((parent, view, position, id) -> {
+                    name = parent.getItemAtPosition(position).toString();
+                    intent.putExtra("routes", value.get(name));
+                    startActivity(intent);
+                });
+
             }
 
             @Override
@@ -76,5 +78,10 @@ public class Routes extends Activity {
 
             }
         });
+//
+//        listView.setOnItemClickListener((parent, view, position, id) -> {
+//            name = parent.getItemAtPosition(position).toString();
+//        });
+
     }
 }
