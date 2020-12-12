595e4fb32a577e8e1bd43f1ac43bd299feb7a105
smirgal00 <georgeadam298@gmail.com>
2020-12-12 19:34:51 +0200

    routes almost done

--- app/src/main/java/com/example/busful/Routes.java
+++ app/src/main/java/com/example/busful/Routes.java
@@ -1,8 +1,18 @@
 package com.example.busful;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Camera;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,30 +22,59 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class Routes extends FragmentActivity implements OnMapReadyCallback {
+import org.w3c.dom.Text;
 
-    GoogleMap map;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Routes extends Activity {
+
+    private GoogleMap map;
+    private DatabaseReference databaseReference;
+    private List<String> routeList;
+    private ListView listView;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(ArrayAdapter.createFromResource(this, R.array.busSpinner,
+                android.R.layout.simple_list_item_1));
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        intent = new Intent(this, Maps.class);
 
-        mapFragment.getMapAsync(this);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String name = parent.getItemAtPosition(position).toString();
+            readData(name);
+            startActivity(intent);
+        });
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
 
-        LatLng belly = new LatLng(46.761107, 23.565309);
-        map.addMarker(new MarkerOptions().position(belly).title("Big Belly!"));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(belly, 15));
-        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+    public void readData(String nr) {
+        databaseReference = FirebaseDatabase.getInstance()
+                .getReference("bus_info")
+                .child(nr);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                intent.putExtra("route", snapshot.getChildren().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
